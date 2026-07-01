ae6f8f45749f338ee1d70835c7a138921808d708
matiastartara <19376841+matiastartara@users.noreply.github.com>
2026-07-01 16:07:30 -0300

    Enhance DirectoryPage with card result handling and update search test

--- pages/DirectoryPage.ts
+++ pages/DirectoryPage.ts
@@ -6,6 +6,7 @@ export class DirectoryPage extends BasePage {
     readonly jobTitle: Locator;
     readonly location: Locator;
     readonly searchButton: Locator;
+    readonly cardResult: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -13,6 +14,7 @@ export class DirectoryPage extends BasePage {
         this.jobTitle = page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').first();
         this.location = page.locator('div:nth-child(3) > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon');
         this.searchButton = page.getByRole('button', { name: 'Search' });
+        this.cardResult = page.locator('.orangehrm-container .orangehrm-directory-card');
     }
 
     async setJobTitle(title: string) {
@@ -27,5 +29,21 @@ export class DirectoryPage extends BasePage {
 
     async search() {
         await this.searchButton.click();
+        await this.page.waitForResponse(response =>
+            response.url().includes('/api/v2/directory/employees') &&
+            response.status() === 200
+        );
+    }
+
+    async getCardResultSize(): Promise<number> {
+        return await this.cardResult.count();
+    }
+
+    getCardResultAt(index: number): Locator {
+        return this.cardResult.nth(index);
+    }
+
+    async getCardHeaderAt(index: number): Promise<string> {
+        return await this.getCardResultAt(index).locator('.orangehrm-directory-card-header').innerText();
     }
 }
